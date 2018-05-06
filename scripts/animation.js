4f415ba0a9cddecddc3198bd8c0f60ea2ddbdb7a
Kasey <Kasey@MacBook-Pro-3.hub>
2018-05-06 20:45:42 +1200

    Animation Finished
    Added box shadow to motion graphics
    Fixed the animation when videos finish
    seems small but was SO annoying.

--- scripts/animation.js
+++ scripts/animation.js
@@ -2,26 +2,30 @@
 $(document).ready(function(){
     //Section One Video
     $('#video1').on('ended',function(){
-      $("#text1").toggleClass('d-none col-lg-6 col-md-6 col-sm-12 col-xs-12');
-      $("#motion1").toggleClass('col col-lg-6 col-md-6 col-sm-12 col-xs-12');
+        $("#text1").toggleClass("check col-lg-6 col-md-6 col-sm-12 col-xs-12", 4500, "linear");
+        $("#text1").css("opacity", "100", 3000);
+        $("#motion1").toggleClass("col col-lg-6 col-md-6 col-sm-12 col-xs-12", 4500);
     });
     
     //Section Two Video
     $('#video2').on('ended',function(){
-      $("#text2").toggleClass('d-none col-lg-6 col-md-6 col-sm-12 col-xs-12');
-      $("#motion2").toggleClass('col col-lg-6 col-md-6 col-sm-12 col-xs-12');
+        $("#text2").toggleClass("check col-lg-6 col-md-6 col-sm-12 col-xs-12", 4500, "linear");
+        $("#text2").css("opacity", "100", 3000);
+        $("#motion2").toggleClass("col col-lg-6 col-md-6 col-sm-12 col-xs-12", 4500);
     });
     
     //Section Three Video
     $('#video3').on('ended',function(){
-      $("#text2").toggleClass('d-none col-lg-6 col-md-6 col-sm-12 col-xs-12');
-      $("#motion3").toggleClass('col col-lg-6 col-md-6 col-sm-12 col-xs-12');
+        $("#text3").toggleClass("check col-lg-6 col-md-6 col-sm-12 col-xs-12", 4500, "linear");
+        $("#text3").css("opacity", "100", 3000);
+        $("#motion3").toggleClass("col col-lg-6 col-md-6 col-sm-12 col-xs-12", 4500);
     });
     
     //Section Four Video
     $('#video4').on('ended',function(){
-      $("#text4").toggleClass('d-none col-lg-6 col-md-6 col-sm-12 col-xs-12');
-      $("#motion4").toggleClass('col col-lg-6 col-md-6 col-sm-12 col-xs-12');
+        $("#text4").toggleClass("check col-lg-6 col-md-6 col-sm-12 col-xs-12", 4500, "linear");
+        $("#text4").css("opacity", "100", 3000);
+        $("#motion4").toggleClass("col col-lg-6 col-md-6 col-sm-12 col-xs-12", 4500);
     });
     
   });
