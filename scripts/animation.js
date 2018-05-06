7c85f63151a1597282b54426022682e7e21f359f
Kasey <Kasey@MacBook-Pro-3.hub>
2018-05-06 15:30:08 +1200

    ANIMATION FUNCTIONALITY DONE!
    FINALLY! The animation functionality is done. Had some trouble with JQuery because I'm completely new to it. As is nearly all code error, I had some syntax stuff wrong and weird things not playing nicely with one another. Now it's fixed: animation.js contains functions which switch the classes on text and motion graphic to make them appear and be equal size. Wahoo!

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,6 +1,27 @@
+//Video Transitioning
 $(document).ready(function(){
-    $('#trig').click(function () {
-    $('#text1').toggleClass('col-lg-6 col-sm-12 col-xs-6');
-    $('#motion1').toggleClass('col-lg-6 col-sm-12 col-xs-6');
+    //Section One Video
+    $('#video1').on('ended',function(){
+      $("#text1").toggleClass('d-none col-lg-6 col-md-6 col-sm-12 col-xs-12');
+      $("#motion1").toggleClass('col col-lg-6 col-md-6 col-sm-12 col-xs-12');
     });
-});
+    
+    //Section Two Video
+    $('#video2').on('ended',function(){
+      $("#text2").toggleClass('d-none col-lg-6 col-md-6 col-sm-12 col-xs-12');
+      $("#motion2").toggleClass('col col-lg-6 col-md-6 col-sm-12 col-xs-12');
+    });
+    
+    //Section Three Video
+    $('#video3').on('ended',function(){
+      $("#text2").toggleClass('d-none col-lg-6 col-md-6 col-sm-12 col-xs-12');
+      $("#motion3").toggleClass('col col-lg-6 col-md-6 col-sm-12 col-xs-12');
+    });
+    
+    //Section Four Video
+    $('#video4').on('ended',function(){
+      $("#text4").toggleClass('d-none col-lg-6 col-md-6 col-sm-12 col-xs-12');
+      $("#motion4").toggleClass('col col-lg-6 col-md-6 col-sm-12 col-xs-12');
+    });
+    
+  });
