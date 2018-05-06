78568a980d8ffc22f8f0c508690bf0bb15ca151e
Kasey <Kasey@MacBook-Pro-3.hub>
2018-05-06 14:35:06 +1200

    Weird
    Trying to make the animations.. It's not working. Saving this before I try a different approach.

---
+++ scripts/animation.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#trig').click(function () {
+    $('#text1').toggleClass('col-lg-6 col-sm-12 col-xs-6');
+    $('#motion1').toggleClass('col-lg-6 col-sm-12 col-xs-6');
+    });
+});
